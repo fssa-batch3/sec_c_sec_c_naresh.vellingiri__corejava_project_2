c77aaf9abb7ab9e8354bdca3af3f951cc9980491
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-08-07 14:30:39 +0530

    project _first_commit

---
+++ src/main/java/com/fssa/ShareToRise/Exceptions/FundraiserExceptions.java
@@ -0,0 +1,19 @@
+package com.fssa.ShareToRise.Exceptions;
+
+public class FundraiserExceptions {
+   
+	
+	public static final String INVALID_ID ="Id can't be negative.";
+
+	public static final String INVALID_TITLE_NAME ="Title Name cannot be null or empty and its length should not be less than 20";
+	
+	public static final String INVALID_DESCRIPTION = "Description can't be null and it should contain minimum of 50 characters length.";
+	
+	public static final String INVALID_DAYS = "Age can't be negative or zero";
+	
+	public static final String Funding_Goal = "Amount can't be negative";
+
+	public static final String Fund_Ending_Date = "Fund ending date cannot be null.";
+
+
+}
