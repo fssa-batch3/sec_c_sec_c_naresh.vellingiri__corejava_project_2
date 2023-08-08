8c3c3754d27a668f3de01087bf5a7a4a6fab36f0
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-08-08 12:33:06 +0530

    new_commit

---
+++ src/main/java/com/fssa/ShareToRise/service/UserService.java
@@ -0,0 +1,29 @@
+package com.fssa.ShareToRise.service;
+
+import com.fssa.ShareToRise.dao.ShareToRiseDao;
+import com.fssa.ShareToRise.model.FundingRaiser;
+import com.fssa.ShareToRise.validator.FundraisingValidator;
+
+import java.sql.SQLException;
+
+public class UserService {
+
+
+	public boolean createFundraiser(FundingRaiser fundraiser) throws SQLException {
+
+		if (FundraisingValidator.validateFundingRaiser(fundraiser)) 
+			return ShareToRiseDao.createTask(fundraiser); 
+			return false; 
+			
+	}
+
+	public boolean updateFundraiser(FundingRaiser fundraiser) throws SQLException {
+
+		if (FundraisingValidator.validateFundingRaiser(fundraiser)) return ShareToRiseDao.updateTask(fundraiser);
+		return false;
+		
+		
+	}
+
+}
+
