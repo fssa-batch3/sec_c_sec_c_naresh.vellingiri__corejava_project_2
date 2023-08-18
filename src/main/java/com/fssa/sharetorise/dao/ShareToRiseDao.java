5cccc34986085ce5fa53474431ff3ebb4450af27
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-08-18 09:37:08 +0530

    -v

---
+++ src/main/java/com/fssa/sharetorise/dao/ShareToRiseDao.java
@@ -0,0 +1,46 @@
+package com.fssa.sharetorise.dao;
+
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.fssa.sharetorise.exceptions.DAOException;
+import com.fssa.sharetorise.model.FundingRaiser;
+import com.fssa.sharetorise.util.ConnectionUtil;
+
+public class ShareToRiseDao {
+
+
+	public boolean createFundraiser(FundingRaiser fundingRaiser) throws DAOException {
+		
+
+		try (Connection con = ConnectionUtil.getMyConnection()) {
+			String sql = "INSERT INTO fundraiser ( Title, Description, FundingGoal, FundEndingDate, NoOfDaysRequired) VALUES (?, ?, ?, ?, ?)";
+
+			try (PreparedStatement preparedStatement = con.prepareStatement(sql)) {
+
+				preparedStatement.setString(1, fundingRaiser.getTitle());
+				preparedStatement.setString(2, fundingRaiser.getDescription());
+				preparedStatement.setDouble(3, fundingRaiser.getFundingGoal());
+				preparedStatement.setObject(4, fundingRaiser.getFundEndingDate());
+				preparedStatement.setInt(5, fundingRaiser.getNoOfDaysRequired());
+
+				preparedStatement.executeUpdate();
+
+
+				return true;
+
+			}
+
+		} catch (SQLException ex) {
+			throw new DAOException("Failed to insert the fundraiser into the database.");
+		}
+
+	}
+	
+	
+	    
+	 
+
+
+}
---
+++ src/main/java/com/fssa/sharetorise/logger/Logger.java
@@ -0,0 +1,19 @@
+package com.fssa.sharetorise.logger;
+
+public class Logger {
+	
+
+	Logger logger = new Logger();
+
+	
+	
+	public void debug(Object obj) {
+		
+		logger.info(obj);
+	}
+
+	public void info(Object obj) {
+		logger.info(obj);
+	}
+
+}
